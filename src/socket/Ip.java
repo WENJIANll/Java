5bea1c894d29a97933d22c6d35a53a9fe3ef117f
WUYNE <1305133324@qq.com>
2020-12-10 22:10:30 +0800

    httpclient

--- src/socket/Ip.java
+++ src/socket/Ip.java
@@ -2,6 +2,7 @@ package socket;
 
 public class Ip {
     public static void main(String[] args) {
+        // ip和子网掩码
         int a = 11111;
         int b = 01010;
         System.out.println((a & b));
