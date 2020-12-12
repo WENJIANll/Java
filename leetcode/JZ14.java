f1036869099f9ef2534626703e48303b4fae14af
WUYNE <1305133324@qq.com>
2020-12-12 22:24:04 +0800

    JZ14

---
+++ leetcode/JZ14.java
@@ -0,0 +1,40 @@
+//
+//
+// 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m-1] 。
+// 请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+//
+//        示例 1：
+//        输入: 2
+//        输出: 1
+//        解释: 2 = 1 + 1, 1 × 1 = 1
+
+//        示例2:
+//        输入: 10
+//        输出: 36
+//        解释: 10 = 3 + 3 + 4, 3 ×3 ×4 = 36
+//
+//        提示：
+//        2 <= n <= 58
+
+
+public class JZ14 {
+    public static void main(String[] args) {
+        JZ14 s = new JZ14();
+        System.out.println(s.cuttingRope(4));
+    }
+
+    public int cuttingRope(int n) {
+        if (n <= 1) return 0;
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        if (n == 4) return 4;
+        int number;
+        int count = 1;
+        while (n >= 5) {
+            count *= 3;
+            n -= 3;
+        }
+        return count * n;
+    }
+
+}
