bc81382b100951de77360c98c3929ced558039fa
WUYNE <1305133324@qq.com>
2020-12-13 21:46:06 +0800

    iterator

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-import set.Main.*;
-import set.PackProperties;
+
 
 public class Main {
     public static void main(String[] args) {
-        int[] list = {1,2,3};
-        System.out.println("Hello World");
-        Hello h = new Hello();
-        h.hello();
-        PackProperties p = new PackProperties();
-        System.out.println(p);
+        int[] list = new int[3];
+        for (int i : list){
+            System.out.println(i);
+        }
+
     }
 }
---
+++ src/set/RunoobTest.java
@@ -0,0 +1,22 @@
+package set;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class RunoobTest {
+    public static void main(String[] args) {
+
+        // 创建集合
+        ArrayList<String> sites = new ArrayList<String>();
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+        sites.add("Zhihu");
+
+        // 获取迭代器
+        Iterator<String> it = sites.iterator();
+
+        // 输出集合中的第一个元素
+        System.out.println(it.next());
+    }
+}
